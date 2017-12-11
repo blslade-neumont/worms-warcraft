b20624358352a6d8a17b5623bca620fd9dcb50b6
Brandon <brandonyoyoslade@gmail.com>
2017-12-10 22:27:11 -0700

    feat(game): add title scene and livecraft font

---
+++ WormsWarcraft/Assets/Behaviors/TitleController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleController : MonoBehaviour
+{
+    [SerializeField] public float flashRate = 1;
+    [SerializeField] public float visibilityPercentage = 0.8f;
+
+    [SerializeField] public Text text;
+
+    [SerializeField] public string sceneName;
+
+    private float aliveTime = 0;
+    
+    private void Start()
+    {
+        if (this.text == null) this.text = this.GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        this.aliveTime += Time.deltaTime;
+        var isVisible = ((this.aliveTime / this.flashRate) % 1) < this.visibilityPercentage;
+        if (this.text != null) this.text.enabled = isVisible;
+
+        if (Input.anyKey && !string.IsNullOrEmpty(this.sceneName)) SceneManager.LoadScene(this.sceneName);
+    }
+}
